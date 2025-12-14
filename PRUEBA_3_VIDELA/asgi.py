520445aedf2a0693b463cd7455ae27718d753150
Felipe Videla <fvidelam@gmail.com>
2025-12-14 14:35:08 -0300

    Inicio del proyecto Prueba 3 Backend Inacap

---
+++ PRUEBA_3_VIDELA/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PRUEBA_3_VIDELA project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PRUEBA_3_VIDELA.settings')
+
+application = get_asgi_application()
