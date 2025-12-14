b28c6e6bba0fc0ca41590bc692e68781c4341f2f
Felipe Videla <fvidelam@gmail.com>
2025-12-14 16:13:11 -0300

    Cree el listado de productos

--- appTienda/urls.py
+++ appTienda/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('productos/', views.productos, name='productos'),
 ]
--- appTienda/views.py
+++ appTienda/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Producto 
+
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def productos(request):
+    lista = Producto.objects.all()
+    data = {
+        'productos': lista
+    }
+    return render(request, 'productos.html', data)
