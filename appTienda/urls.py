e5d6fadcde3e1c69f59bb21c06331c1e15b4eeb7
Felipe Videla <fvidelam@gmail.com>
2025-12-15 00:39:45 -0300

    Seguimiento de pedido por token

--- appTienda/urls.py
+++ appTienda/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('productos/', views.productos, name='productos'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('categorias/<int:id>/', views.productos_por_categoria, name='productos_por_categoria'),
+    path('pedir/', views.pedir, name= 'pedir'),
+    path('seguimiento/<uuid:token>/', views.seguimiento, name='seguimiento'),
 ]
--- appTienda/views.py
+++ appTienda/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Producto 
+from .models import Producto, Categoria, Pedido 
+from .forms import PedidoForm 
 
 
 # Create your views here.
@@ -13,3 +14,42 @@ def productos(request):
         'productos': lista
     }
     return render(request, 'productos.html', data)
+
+def categorias(request):
+    lista = Categoria.objects.all()
+    data = {
+        'categorias': lista
+    }
+    return render(request, 'categorias.html', data)
+
+def productos_por_categoria(request, id):
+    cat = Categoria.objects.get(id=id)
+    lista = Producto.objects.filter(categoria=cat)
+    data = {
+        'categoria': cat, 
+        'productos': lista
+    }
+    return render(request, 'productos_categoria.html',data)
+
+def pedir(request):
+    data = {
+        'form': PedidoForm()
+    }
+
+    if request.method == 'POST': 
+        formulario = PedidoForm(request.POST)
+        if formulario.is_valid(): 
+            formulario.save()
+            data['mensaje'] = 'Pedido enviado'
+            data['form'] = PedidoForm() #Esto es para limpiar el formulario
+        else: 
+            data['form'] = formulario 
+    
+    return render(request, 'pedido_form.html', data)
+
+def seguimiento(request, token): 
+    pedido = Pedido.objects.get(token=token)
+    data = {
+        'pedido': pedido 
+    }
+    return render(request, 'seguimiento.html', data)
